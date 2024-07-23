2fc11f6bd2c6acd20fc5580064b3e4b2bc230a89
wa1ead <waliddraa2@gmail.com>
2024-07-23 13:17:14 +0100

    create Card component: ReactJs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import Card from "./components/Card";
 import NavBar from "./components/NavBar";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
 
 const App = () => {
   return (
     <div className="App">
       <NavBar />
+      <Container>
+        <Grid container spacing={5}>
+          <Card />
+        </Grid>
+      </Container>
     </div>
   );
 };
---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+
+const Card = () => {
+  return (
+    <Grid item xs={3}>
+      <Paper elevation={3}>CARD</Paper>
+    </Grid>
+  );
+};
+
+export default Card;
