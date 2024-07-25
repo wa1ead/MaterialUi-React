87ac9193026029541dab598e0feaaf5262f07654
wa1ead <waliddraa2@gmail.com>
2024-07-25 12:51:41 +0100

    minimize the font size: ThemeProvider

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Rating from "@mui/material/Rating";
 import { AccessTime } from "@mui/icons-material";
-import { createTheme } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material";
 
 const theme = createTheme({
   components: {
@@ -35,55 +35,57 @@ const theme = createTheme({
 const Card = () => {
   return (
     <Grid item xs={3}>
-      <Paper elevation={3}>
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0qyQLFvNh7o1xuNyEu9QOFEd34jJR-rZ46g&s"
-          alt="SYDNEY"
-          className="img"
-        />
-        <Box paddingX={1}>
-          <Typography variant="subtitle1" component="h2">
-            SYDNEY
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-          }}
-        >
-          <AccessTime sx={{ width: 12.5 }} />
-          <Typography variant="body2" component="p" marginLeft={0.5}>
-            5 hours
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            marginTop: "12px",
-          }}
-        >
-          <Rating
-            name="read-only"
-            value={4.5}
-            precision={0.5}
-            readOnly
-            size="small"
+      <ThemeProvider theme={theme}>
+        <Paper elevation={3}>
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0qyQLFvNh7o1xuNyEu9QOFEd34jJR-rZ46g&s"
+            alt="SYDNEY"
+            className="img"
           />
-          <Typography variant="body2" component="p" marginLeft={0.5}>
-            4.5
-          </Typography>
-          <Typography variant="body3" component="p" marginLeft={1.5}>
-            (656 reviews)
-          </Typography>
-        </Box>
-        <Box>
-          <Typography variant="h6" component="h3" marginTop={0}>
-            From $145
-          </Typography>
-        </Box>
-      </Paper>
+          <Box paddingX={1}>
+            <Typography variant="subtitle1" component="h2">
+              SYDNEY
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <AccessTime sx={{ width: 12.5 }} />
+            <Typography variant="body2" component="p" marginLeft={0.5}>
+              5 hours
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              marginTop: "12px",
+            }}
+          >
+            <Rating
+              name="read-only"
+              value={4.5}
+              precision={0.5}
+              readOnly
+              size="small"
+            />
+            <Typography variant="body2" component="p" marginLeft={0.5}>
+              4.5
+            </Typography>
+            <Typography variant="body3" component="p" marginLeft={1.5}>
+              (656 reviews)
+            </Typography>
+          </Box>
+          <Box>
+            <Typography variant="h6" component="h3" marginTop={0}>
+              From $145
+            </Typography>
+          </Box>
+        </Paper>
+      </ThemeProvider>
     </Grid>
   );
 };
