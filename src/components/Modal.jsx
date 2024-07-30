a60ffc6bfecb88c559d8660123a5a0e989f67121
wa1ead <waliddraa2@gmail.com>
2024-07-30 12:36:45 +0100

    create Modal component: MaterialUI

---
+++ src/components/Modal.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import DatePicker from "./DatePicker";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+function BasicModal() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Button onClick={handleOpen}>Book now</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <DatePicker />
+        </Box>
+      </Modal>
+    </div>
+  );
+}
+
+export default BasicModal;
--- src/pages/Tour.jsx
+++ src/pages/Tour.jsx
@@ -1,4 +1,53 @@
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import Typography from "@mui/material/Typography";
+import ImageCollage from "../components/ImageCollage";
+import CustomizedAccordions from "../components/Accordion";
+import BasicModal from "../components/Modal";
+
 const Tour = () => {
-  return <div>Hello</div>;
+  return (
+    <Container sx={{ width: 900 }}>
+      <Typography variant="h3" component="h1" marginTop={3}>
+        Explore the World
+      </Typography>
+      <Box marginTop={3} sx={{ display: "flex" }}>
+        <img
+          src="https://imageio.forbes.com/specials-images/imageserve/656df61cc3a44648c235dde3/Las-Vegas--Nevada--USA-at-the-Welcome-Sign/960x0.jpg?format=jpg&width=960"
+          alt="Las Vegas"
+          height={325}
+        />
+        <ImageCollage />
+      </Box>
+      <Box>
+        <Typography component="h4" variant="h6" marginTop={3}>
+          About this ticket
+        </Typography>
+        <Typography component="p" variant="paragraph" marginTop={3}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste a
+          explicabo qui laboriosam, suscipit quo. Ducimus fuga debitis dolor
+          error exercitationem odio! Accusantium iusto debitis, placeat
+          aspernatur cumque minima nemo culpa autem illo, nobis fuga
+          reprehenderit animi tempora blanditiis veniam.
+        </Typography>
+      </Box>
+      <Box>
+        <Typography component="h4" variant="h6" marginTop={3} marginBottom={2}>
+          Frequently asked Questions
+        </Typography>
+        <CustomizedAccordions />
+      </Box>
+      <Paper
+        sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+        elevation={3}
+      >
+        <BottomNavigation>
+          <BasicModal />
+        </BottomNavigation>
+      </Paper>
+    </Container>
+  );
 };
 export default Tour;
